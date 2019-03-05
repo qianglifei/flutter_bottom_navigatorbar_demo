94c59d9b4895c780ea48509c7918085131efac40
qianglifei <qianglifei@gmail.com>
2019-03-05 21:53:02 +0800

    不规则的底部导航栏

--- lib/bottom_navigatorbar_demo.dart
+++ lib/bottom_navigatorbar_demo.dart
@@ -1,17 +1,39 @@
 import 'package:flutter/material.dart';
+import 'each_view.dart';
 
 class BottomAppBarDemo extends StatefulWidget{
    _BottomAppBarDemoState createState() => _BottomAppBarDemoState();
 }
 
 class _BottomAppBarDemoState extends State<BottomAppBarDemo>{
+  /// 创建视图数组
+  List<Widget> _eachView;
+  ///根据索引来改变视图
+  int _index = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+        _eachView = List();
+        _eachView..add(EachView('Home'))..add(EachView('Me'));
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+
+        body: _eachView[_index],
         floatingActionButton: new FloatingActionButton(
             onPressed: (){
-
+              Navigator.of(context).push(
+                  MaterialPageRoute(
+                     builder: (BuildContext context){
+                        return EachView('New Page');
+                     }
+                  )
+              );
             },
             tooltip: '草拟妹',
             child: Icon(
@@ -33,7 +55,9 @@ class _BottomAppBarDemoState extends State<BottomAppBarDemo>{
                        ),
                        color: Colors.greenAccent,
                        onPressed:(){
-
+                          setState(() {
+                            _index = 0;
+                          });
                        },
                     ),
                     IconButton(
@@ -42,7 +66,9 @@ class _BottomAppBarDemoState extends State<BottomAppBarDemo>{
                       ),
                       color: Colors.pink,
                       onPressed: (){
-
+                          setState(() {
+                            _index = 1;
+                          });
                       },
                     )
                 ],
